e29d3df8907d5563c194462f4bd4231a0017ec26
Abdul <32986255+abdulhub@users.noreply.github.com>
2019-09-04 19:43:11 +0530

    Update connect2dbandkeyvault.ps1

--- src/main/resources/connect2dbandkeyvault.ps1
+++ src/main/resources/connect2dbandkeyvault.ps1
@@ -3,13 +3,13 @@
 # Create a column master key in Azure Key Vault and encrypt the column
 
 # Azure subscription and service princial settings 
-$applicationId = "3783a663-8ac1-4b8f-a539-7e15ebbc43c8";
-$secret = "nMDBVdvXTGbkjY4[JwsrfmVtEg1]3?8-"
-$tenantId = "0ae51e19-07c8-4e4b-bb6d-648ee58410f4";
-$SubscriptionId = "7de3b70a-c480-4eaf-baf8-112825f9f5ab"
+$applicationId = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+$secret = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+$tenantId = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+$SubscriptionId = "xxxxxxxxxxxxxxxxxxxxxxxxxx"
 
 # Azure Key Vault settings
-$akvName = "stepcount"
+$akvName = "xxxxxxxxxxxxx"
 $akvKeyName = "PoshKey1"
 
 # Column Encryption Key and Master key Name in the DB
@@ -21,9 +21,9 @@ $col2 = "dbo.testtable.ssn"
 
 
 # Connect to your database (Azure SQL database).
-$serverName = "KOR1101123"
+$serverName = "xxxxxxxxxxxx"
 $databaseName = "testdb"
-$dbpassword = "abduladmin"
+$dbpassword = "xxxxxxxx"
 $dbuser = "abdul"
 $sqlConnectionString = "Data Source=$servername;Initial Catalog=$databasename;User ID=$dbuser;Password=$dbpassword;MultipleActiveResultSets=False;Connect Timeout=30;Encrypt=True;TrustServerCertificate=False;Column Encryption Setting=Enabled"
 $connection = New-Object Microsoft.SqlServer.Management.Common.ServerConnection
