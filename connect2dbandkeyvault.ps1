340ed9bdd4216747e1fa33d7e2c14de43f51fef2
Abdul <32986255+abdulhub@users.noreply.github.com>
2019-09-09 23:19:52 +0530

    Update connect2dbandkeyvault.ps1

--- connect2dbandkeyvault.ps1
+++ connect2dbandkeyvault.ps1
@@ -3,10 +3,10 @@
 # Create a column master key in Azure Key Vault and encrypt the column
 
 # Azure subscription and service princial settings 
-$applicationId = "3783a663-8ac1-4b8f-a539-7e15ebbc43c8";
-$secret = "nMDBVdvXTGbkjY4[JwsrfmVtEg1]3?8-"
-$tenantId = "0ae51e19-07c8-4e4b-bb6d-648ee58410f4";
-$SubscriptionId = "7de3b70a-c480-4eaf-baf8-112825f9f5ab"
+$applicationId = "xxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+$secret = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+$tenantId = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+$SubscriptionId = "xxxxxxxxxxxxxxxxxxxxxx"
 
 # Azure Key Vault settings
 $akvName = "stepcount"
@@ -21,7 +21,7 @@ $col2 = "dbo.testtable.ssn"
 
 
 # Connect to your database (Azure SQL database).
-$serverName = "KOR1101123"
+$serverName = "xxxxxxxxxxxx"
 $databaseName = "testdb"
 $dbpassword = "abduladmin"
 $dbuser = "abdul"
